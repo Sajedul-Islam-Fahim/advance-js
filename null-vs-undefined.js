9fd022423f44fd3a035f9a52e40f29a094edff8f
Sajedul Islam Fahim <sajedulislamfahim@gmail.com>
2021-09-11 10:46:45 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,31 @@
+//First way :
+// let pakhi;
+// console.log(pakhi);
+
+//Second way:
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(13, 8);
+// console.log(result);
+
+//Third way:
+//  function add(num1, num2) {
+//     console.log(num1, num2);
+//  }
+// const result = add(13);
+// console.log(result);
+
+//fourth way:
+// const premik = {name: "smart dude", phone:458621};
+// console.log(premik.gf);
+
+//fifth way:
+// let fun = undefined;
+// console.log(fun);
+
+//sixth way:
+// let ages = [2, 5, 7];
+// console.log(ages[11]);
+
