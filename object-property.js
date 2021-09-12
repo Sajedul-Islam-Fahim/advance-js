8f246f11e7959b5c47adbcf882d6cb1971e580d8
Sajedul Islam Fahim <sajedulislamfahim@gmail.com>
2021-09-12 07:46:05 +0600

    map, filter, find used to find object property

---
+++ object-property.js
@@ -0,0 +1,13 @@
+const students = [
+    {id: 21, name: 'Jhankar Mahbub'},
+    {id: 31, name: 'Anisul Islam'},
+    {id: 41, name: 'The New Boston'},
+    {id: 51, name: 'Ten Minute School'}
+];
+
+const studentName = students.map(x => x.name);
+const fortyPlus = students.filter(x => x.id > 40);
+const biggerOne = students.find(x => x.id > 40);
+console.log(fortyPlus);
+console.log(studentName);
+console.log(biggerOne);
