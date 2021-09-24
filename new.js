2c90d50abe06726297b05ae157a85d80b627b187
Sajedul Islam Fahim <sajedulislamfahim@gmail.com>
2021-09-24 06:49:32 +0600

    new added

---
+++ new.js
@@ -0,0 +1,13 @@
+class Person{
+    constructor(firstName, lastName, salary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+}
+const heroPerson = new Person('Hero', 'Balam', 20000);
+console.log(heroPerson); 
+const friendlyPerson = new Person('Hero', 'Jamal', 15000);
+console.log(friendlyPerson);
+
+//class theke object bananur jnno new keyword er use hoy. jate sobgula object er moddhe property er behaviour er similarity thake.   
